3d4915713457d75746b0ca4d8213a14b02659eb3
Pathikrit Chanda <Pathikrit.Chanda@brillio.com>
2022-09-14 23:35:28 +0530

    added product api

---
+++ src/main/java/com/handykrafts/products/Product.java
@@ -0,0 +1,91 @@
+package com.handykrafts.products;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Product")
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	private int productId;
+	private String productName;
+	private String productDescription;
+	private String productPrice;
+	private String productImage;
+	
+	private int categoryId;
+	
+	public Product() {
+		super();
+	}
+	
+
+	public Product(int productId, String productName, String productDescription, String productPrice,
+			String productImage, int categoryId) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productDescription = productDescription;
+		this.productPrice = productPrice;
+		this.productImage = productImage;
+		this.categoryId = categoryId;
+	}
+
+
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public String getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(String productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public String getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	
+
+	
+}
---
+++ src/main/java/com/handykrafts/products/ProductDao.java
@@ -0,0 +1,7 @@
+package com.handykrafts.products;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductDao extends CrudRepository<Product, Integer>{
+	
+}
