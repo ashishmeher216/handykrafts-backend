f32c1c445f55fe3550b0f7befd946159a93703aa
Pathikrit Chanda <Pathikrit.Chanda@BRILLIO.COM>
2022-09-15 23:34:58 +0530

    updated product api

--- src/main/java/com/handykrafts/products/Product.java
+++ src/main/java/com/handykrafts/products/Product.java
@@ -3,6 +3,7 @@ package com.handykrafts.products;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,7 +12,7 @@ import javax.persistence.Table;
 public class Product {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int productId;
 	private String productName;
 	private String productDescription;
--- src/main/java/com/handykrafts/products/ProductController.java
+++ src/main/java/com/handykrafts/products/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
 	@Autowired
